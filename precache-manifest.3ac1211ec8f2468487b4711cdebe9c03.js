e3e3e4328b42acd71d03dbecc7035b2b84ed61d3
bkallaus <benkallaus@gmail.com>
2020-04-28 20:23:21 -0500

    Updates

---
+++ precache-manifest.3ac1211ec8f2468487b4711cdebe9c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ce20dfd6ea5a7a6d41dca4097480d61",
+    "url": "/musical-cards/index.html"
+  },
+  {
+    "revision": "5327727ce9b4268e103e",
+    "url": "/musical-cards/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e52aae7458628e716720",
+    "url": "/musical-cards/static/js/2.83b2148e.chunk.js"
+  },
+  {
+    "revision": "4d3d17ca1d72509d468eda11f7e92a3e",
+    "url": "/musical-cards/static/js/2.83b2148e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5327727ce9b4268e103e",
+    "url": "/musical-cards/static/js/main.b5124d9a.chunk.js"
+  },
+  {
+    "revision": "04ead4ebe55bbccf7e73",
+    "url": "/musical-cards/static/js/runtime-main.f48f0d37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/musical-cards/precache-manifest.8cf43978dfa7ed1d32683781ce3c579c.js"
+  "/musical-cards/precache-manifest.3ac1211ec8f2468487b4711cdebe9c03.js"
 );
 
 self.addEventListener('message', (event) => {
